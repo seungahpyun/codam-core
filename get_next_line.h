8bf78aa1d0577847930c151a14e008ae976c8b41
Seunga Pyun <spyun@student.codam.nl>
2024-10-28 09:56:32 +0100

    add struct

--- get_next_line.h
+++ get_next_line.h
@@ -13,4 +13,14 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}	t_list;
+
 #endif
