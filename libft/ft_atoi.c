9cca8ed30d63ed0bd0a66b01b517454d0a7e78b0
Seunga Pyun <spyun@student.codam.nl>
2024-10-29 09:21:32 +0100

    ft_atoi handle overflow

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -11,19 +11,42 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
+
+static size_t	skip_whitespace(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	return (i);
+}
+
+static int	check_overflow(long result, int sign, char c)
+{
+	if ((result > LONG_MAX / 10)
+		|| (result == LONG_MAX / 10 && (c - '0') > LONG_MAX % 10))
+	{
+		if (sign == 1)
+			return (-1);
+		else
+			return (0);
+	}
+	return (1);
+}
 
 int	ft_atoi(const char *str)
 {
 	size_t	i;
 	long	result;
 	int		sign;
+	int		overflow_check;
 
-	i = 0;
+	i = skip_whitespace(str);
 	result = 0;
 	sign = 1;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-		||str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
@@ -32,6 +55,9 @@ int	ft_atoi(const char *str)
 	}
 	while (ft_isdigit(str[i]))
 	{
+		overflow_check = check_overflow(result, sign, str[i]);
+		if (overflow_check != 1)
+			return (overflow_check);
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
