bff595f980dbfe5fdd11646c7173e960bd09cabc
seungahpyun <seungahpyun@icloud.com>
2024-11-20 17:07:27 +0100

    file sorted

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,15 +6,23 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/20 16:33:42 by spyun         #+#    #+#                 */
-/*   Updated: 2024/11/20 16:35:06 by spyun         ########   odam.nl         */
+/*   Updated: 2024/11/20 17:05:14 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <stdbool.h>
+# include <limits.h>
 # include "libft/libft.h"
 
-#endif
+typedef struct s_stack
+{
+	long			nbr;
+	long			index;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}	t_stack;
 
+#endif
