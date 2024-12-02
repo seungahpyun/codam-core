52b3aab175bc386336a2668b83785b7eeca45a06
Seunga Pyun <spyun@student.codam.nl>
2024-12-02 17:03:16 +0100

    clean ver

--- srcs/input_handler.c
+++ srcs/input_handler.c
@@ -6,13 +6,37 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/11/21 08:53:50 by spyun         #+#    #+#                 */
-/*   Updated: 2024/11/29 15:25:08 by spyun         ########   odam.nl         */
+/*   Updated: 2024/12/02 15:58:47 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+static int	check_int_range(const char *str)
+{
+	long	num;
+	int		sign;
+	int		i;
+
+	num = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	num *= sign;
+	return (num <= INT_MAX && num >= INT_MIN);
+}
 
 t_stack	*ft_process_single_input(char **argv)
 {
@@ -28,7 +52,8 @@ t_stack	*ft_process_single_input(char **argv)
 		ft_error();
 	while (split_array[i])
 	{
-		if (!is_valid_number(split_array[i]))
+		if (!is_valid_number(split_array[i])
+			|| !check_int_range(split_array[i]))
 		{
 			ft_free(&stack_a);
 			ft_free_split(split_array);
@@ -51,20 +76,17 @@ t_stack	*ft_parse_input(int argc, char **argv)
 	stack_a = NULL;
 	i = 1;
 	if (argc == 2)
-		stack_a = ft_process_single_input(argv);
-	else
+		return (ft_process_single_input(argv));
+	while (i < argc)
 	{
-		while (i < argc)
+		if (!is_valid_number(argv[i]) || !check_int_range(argv[i]))
 		{
-			if ((!is_valid_number(argv[i])))
-			{
-				ft_free(&stack_a);
-				ft_error();
-			}
-			j = ft_atoi(argv[i]);
-			ft_stack_add_back(&stack_a, ft_stack_new(j));
-			i++;
+			ft_free(&stack_a);
+			ft_error();
 		}
+		j = ft_atoi(argv[i]);
+		ft_stack_add_back(&stack_a, ft_stack_new(j));
+		i++;
 	}
 	return (stack_a);
 }
