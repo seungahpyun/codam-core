e32f95e2787cb6f78d05f54ad46ddaafa8bd8dfa
Seunga Pyun <spyun@student.codam.nl>
2024-12-05 15:02:39 +0100

    working ver

--- pipex/srcs/cmd_parser.c
+++ pipex/srcs/cmd_parser.c
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/04 18:39:44 by spyun         #+#    #+#                 */
-/*   Updated: 2024/12/04 18:50:00 by spyun         ########   odam.nl         */
+/*   Updated: 2024/12/05 15:01:33 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,62 +14,139 @@
 #include "../libft/libft.h"
 #include <stdlib.h>
 
-static int	is_quote(char c)
+static int count_args(char *str)
 {
-	return (c == '"' || c == '\'');
+	int count;
+	int i;
+	char quote;
+	int in_word;
+
+	count = 0;
+    i = 0;
+    in_word = 0;
+    while (str[i])
+    {
+        if (str[i] == '"' || str[i] == '\'')
+        {
+            quote = str[i++];
+            if (!in_word)
+                count++;
+            in_word = 1;
+            while (str[i])
+            {
+                if (str[i] == '\\' && str[i + 1] == quote)
+                    i += 2;
+                else if (str[i] == quote)
+                    break;
+                else
+                    i++;
+            }
+            if (str[i])
+                i++;
+        }
+        else if (str[i] != ' ')
+        {
+            if (!in_word)
+                count++;
+            in_word = 1;
+            i++;
+        }
+        else
+        {
+            in_word = 0;
+            i++;
+        }
+    }
+    return (count);
 }
 
-static char	*parse_quoted_arg(char *str, int *i)
+static char *copy_quoted_content(char *str, int *i, char quote)
 {
-	char	quote;
-	int		start;
+    int start;
+    int j;
+    char *result;
+    char *temp;
 
-	quote = str[*i];
-	start = ++(*i);
-	while (str[*i] && str[*i] != quote)
-		(*i)++;
-	if (!str[*i])
-		return (NULL);
-	str[*i] = '\0';
-	return (ft_strdup(&str[start]));
+    start = *i;
+    j = 0;
+    temp = malloc(ft_strlen(str) + 1);
+    if (!temp)
+        return (NULL);
+    while (str[*i])
+    {
+        if (str[*i] == '\\' && str[*i + 1] == quote)
+        {
+            temp[j++] = str[*i + 1];
+            *i += 2;
+        }
+        else if (str[*i] == quote)
+            break;
+        else
+            temp[j++] = str[(*i)++];
+    }
+    temp[j] = '\0';
+    result = ft_strdup(temp);
+    free(temp);
+    if (str[*i])
+        (*i)++;
+    return (result);
 }
 
-char	**parse_cmd(char *str)
+static char *extract_quoted(char *str, int *i)
 {
-	char	**args;
-	char	*token;
-	int		i;
-	int		arg_count;
-	int		len;
+    char quote;
 
-	i = 0;
-	arg_count = 0;
-	len = ft_strlen(str);
-	args = malloc(sizeof(char *) * (len + 1));
-	if (!args)
-		return (NULL);
-	while (str[i])
-	{
-		while (str[i] && str[i] == ' ')
-			i++;
-		if (!str[i])
-			break ;
-		if (is_quote(str[i]))
-			token = parse_quoted_arg(str, &i);
-		else
-		{
-			token = ft_strdup(&str[i]);
-			while (str[i] && str[i] != ' ')
-				i++;
-			token[i] = '\0';
-		}
-		if (!token)
-		{
-			free_array(args);
-			return (NULL);
-		}
-		args[arg_count++] = token;
-	}
-	args[arg_count] = NULL;
-	return (args);
+    quote = str[*i];
+    (*i)++;
+    return (copy_quoted_content(str, i, quote));
+}
+
+static char *extract_unquoted(char *str, int *i)
+{
+    int start;
+    int len;
+    char *result;
+
+    start = *i;
+    while (str[*i] && str[*i] != ' ' && str[*i] != '"' && str[*i] != '\'')
+        (*i)++;
+    len = *i - start;
+    result = malloc(len + 1);
+    if (!result)
+        return (NULL);
+    ft_strlcpy(result, &str[start], len + 1);
+    return (result);
+}
+
+char **parse_cmd(char *str)
+{
+    char **args;
+    char *token;
+    int i;
+    int arg_count;
+
+    args = malloc(sizeof(char *) * (count_args(str) + 1));
+    if (!args)
+        return (NULL);
+    i = 0;
+    arg_count = 0;
+    while (str[i])
+    {
+        while (str[i] && str[i] == ' ')
+            i++;
+        if (!str[i])
+            break;
+        if (str[i] == '"' || str[i] == '\'')
+            token = extract_quoted(str, &i);
+        else
+            token = extract_unquoted(str, &i);
+        if (!token)
+        {
+            free_array(args);
+            return (NULL);
+        }
+        args[arg_count++] = token;
+    }
+    args[arg_count] = NULL;
+    return (args);
 }
--- pipex/srcs/utils.c
+++ pipex/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/04 09:58:18 by spyun         #+#    #+#                 */
-/*   Updated: 2024/12/05 10:07:49 by spyun         ########   odam.nl         */
+/*   Updated: 2024/12/05 15:00:38 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,86 +16,102 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void	error_exit(void)
+void error_exit(void)
 {
-	perror("\033[31mError");
-	exit(EXIT_FAILURE);
+    perror("\033[31mError");
+    exit(EXIT_FAILURE);
 }
 
-void	free_array(char **array)
+void free_array(char **array)
 {
-	int	i;
+    int i;
 
-	if (!array)
-		return ;
-	i = 0;
-	while (array[i])
-		free(array[i++]);
-	free(array);
+    if (!array)
+        return ;
+    i = 0;
+    while (array[i])
+        free(array[i++]);
+    free(array);
 }
 
-char	*get_cmd_path(char **paths, char *cmd)
+char *get_cmd_path(char **paths, char *cmd)
 {
-	char	*path;
-	char	*part_path;
-	int		i;
+    char *path;
+    char *part_path;
+    int i;
 
-	i = 0;
-	while (paths[i])
-	{
-		part_path = ft_strjoin(paths[i], "/");
-		path = ft_strjoin(part_path, cmd);
-		free(part_path);
-		if (access(path, F_OK | X_OK) == 0)
-		{
-			while (paths[i])
-				free(paths[i++]);
-			free(paths);
-			return (path);
-		}
-		free(path);
-		i++;
-	}
-	return (NULL);
+    i = 0;
+    if (cmd[0] == '/' || (cmd[0] == '.' && cmd[1] == '/'))
+    {
+        if (access(cmd, F_OK | X_OK) == 0)
+            return (ft_strdup(cmd));
+        return (NULL);
+    }
+    while (paths && paths[i])
+    {
+        part_path = ft_strjoin(paths[i], "/");
+        if (!part_path)
+            return (NULL);
+        path = ft_strjoin(part_path, cmd);
+        free(part_path);
+        if (!path)
+            return (NULL);
+        if (access(path, F_OK | X_OK) == 0)
+        {
+            free_array(paths);
+            return (path);
+        }
+        free(path);
+        i++;
+    }
+    if (paths)
+        free_array(paths);
+    return (NULL);
 }
 
-char	*find_path(char *cmd, char **envp)
+char *find_path(char *cmd, char **envp)
 {
-	char	**paths;
-	char	*path;
+    char **paths;
+    char *path;
 
-	while (*envp && ft_strncmp("PATH=", *envp, 5))
-		envp++;
-	if (!*envp)
-		return (NULL);
-	paths = ft_split(*envp + 5, ':');
-	if (!paths)
-		return (NULL);
-	path = get_cmd_path(paths, cmd);
-	return (path);
+    if (!cmd || !cmd[0])
+        return (NULL);
+    while (envp && *envp && ft_strncmp("PATH=", *envp, 5))
+        envp++;
+    if (!envp || !*envp)
+    {
+        if (access(cmd, F_OK | X_OK) == 0)
+            return (ft_strdup(cmd));
+        return (NULL);
+    }
+    paths = ft_split(*envp + 5, ':');
+    if (!paths)
+        return (NULL);
+    path = get_cmd_path(paths, cmd);
+    return (path);
 }
 
-void	execute_cmd(char *cmd, char **envp)
+void execute_cmd(char *cmd, char **envp)
 {
-	char	**cmd_args;
-	char	*cmd_path;
+    char **cmd_args;
+    char *cmd_path;
 
-	cmd_args = parse_cmd(cmd);
-	if (!cmd_args)
-		error_exit();
-	if (!cmd_args[0])
-	{
-		free_array(cmd_args);
-		error_exit();
-	}
-	cmd_path = find_path(cmd_args[0], envp);
-	if (!cmd_path)
-	{
-		free_array(cmd_args);
-		error_exit();
-	}
-	execve(cmd_path, cmd_args, envp);
-	free(cmd_path);
-	free_array(cmd_args);
-	error_exit();
+    cmd_args = parse_cmd(cmd);
+    if (!cmd_args || !cmd_args[0])
+    {
+        free_array(cmd_args);
+        error_exit();
+    }
+    cmd_path = find_path(cmd_args[0], envp);
+    if (!cmd_path)
+    {
+        free_array(cmd_args);
+        error_exit();
+    }
+    if (execve(cmd_path, cmd_args, envp) == -1)
+    {
+        free(cmd_path);
+        free_array(cmd_args);
+        error_exit();
+    }
 }
