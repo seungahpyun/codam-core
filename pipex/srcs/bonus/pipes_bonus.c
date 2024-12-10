8ffc1c89725d874dd928b982b0fd8d4ff825de49
seungahpyun <seungahpyun@icloud.com>
2024-12-10 08:12:23 +0100

    adding leak blocks

--- pipex/srcs/bonus/pipes_bonus.c
+++ pipex/srcs/bonus/pipes_bonus.c
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/09 07:15:03 by spyun         #+#    #+#                 */
-/*   Updated: 2024/12/09 09:57:15 by spyun         ########   odam.nl         */
+/*   Updated: 2024/12/10 08:10:09 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,19 @@ void	create_pipes(t_pipex *pipex)
 	{
 		pipex->pipes[i] = malloc(sizeof(int) * 2);
 		if (!pipex->pipes[i])
+		{
+			while (--i >= 0)
+				free(pipex->pipes[i]);
+			free(pipex->pipes);
 			error_exit("Malloc failed");
+		}
 		if (pipe(pipex->pipes[i]) == -1)
+		{
+			while (i >= 0)
+				free(pipex->pipes[i--]);
+			free(pipex->pipes);
 			error_exit("Pipe failed");
+		}
 		i++;
 	}
 }
