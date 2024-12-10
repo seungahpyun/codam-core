39fbca3e409c8740f9744fe1f1c224f709034cbb
seungahpyun <seungahpyun@icloud.com>
2024-12-10 09:18:18 +0100

    norm

--- pipex/srcs/bonus/main_bonus.c
+++ pipex/srcs/bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/09 07:12:45 by spyun         #+#    #+#                 */
-/*   Updated: 2024/12/10 09:14:59 by spyun         ########   odam.nl         */
+/*   Updated: 2024/12/10 09:18:09 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,16 @@ int	main(int argc, char **argv, char **envp)
 	t_pipex	pipex;
 	int		status;
 	int		exit_status;
-	int		i;
 
 	status = 0;
 	exit_status = 0;
 	if (argc < 5)
-		error_exit("Usage: ./pipex file1 cmd1 cmd2 cmd3 ... cmdn file2", &pipex);
+		error_exit("Usage: ./pipex file1 cmd1 cmd2 cmd3 \
+		... cmdn file2", &pipex);
 	init_pipex(&pipex, argc, argv);
 	create_pipes(&pipex);
 	execute_commands(&pipex, envp);
 	cleanup_pipex(&pipex);
-	i = 3;
-	while (i < 256)
-		close(i++);
 	while (wait(&status) > 0)
 	{
 		if (WIFSIGNALED(status))
