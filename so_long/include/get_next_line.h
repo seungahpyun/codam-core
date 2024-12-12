8d03c3040489fbec4460c1e56cf912f15410e98b
Seunga Pyun <spyun@student.codam.nl>
2024-12-12 12:21:47 +0100

    structure

---
+++ so_long/include/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spyun <marvin@42.fr>                         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/10/24 11:16:58 by spyun         #+#    #+#                 */
+/*   Updated: 2024/12/12 12:10:02 by spyun         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+typedef struct s_gnl_list
+{
+	int					fd;
+	char				*buffer;
+	struct s_gnl_list	*next;
+}	t_gnl_list;
+
+char	*get_next_line(int fd);
+
+#endif
---
+++ so_long/src/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spyun <spyun@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/12/12 11:56:04 by spyun         #+#    #+#                 */
+/*   Updated: 2024/12/12 12:18:12 by spyun         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+int	main(int argc, char **argv)
+{
+	t_game	game;
+	
+	check_args(argc, argv);
+	ft_memset(&game, 0, sizeof(t_game));
+	if (init_game(&game, argv[1]) == -1)
+	{
+		ft_putstr_fd("Error\n", 2);
+		exit_game(&game, 1);
+	}
+	mlx_loop(game.mlx);
+	return (0);
+}
