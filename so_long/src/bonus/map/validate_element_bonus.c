2a1d4f60a34f1ebb4a6b1188927b2ee6cc3772df
seungahpyun <seungahpyun@icloud.com>
2024-12-29 13:15:06 +0100

    clean up

--- so_long/src/bonus/map/validate_element_bonus.c
+++ so_long/src/bonus/map/validate_element_bonus.c
@@ -6,7 +6,7 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/13 10:44:27 by spyun         #+#    #+#                 */
-/*   Updated: 2024/12/24 13:37:57 by spyun         ########   odam.nl         */
+/*   Updated: 2024/12/29 13:13:08 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,66 @@ static bool	update_element_count(t_game *game, char element, int x, int y)
 	if (player_count > 1 || exit_count > 1)
 		return (false);
 	return (true);
+}
+
+static bool	check_element_requirements(bool player_found, bool exit_found,
+		t_game *game)
+{
+	if (!player_found)
+	{
+		ft_putendl_fd("Error: No player found", STDERR_FILENO);
+		return (false);
+	}
+	if (!exit_found)
+	{
+		ft_putendl_fd("Error: No exit found", STDERR_FILENO);
+		return (false);
+	}
+	if (game->collectibles == 0)
+	{
+		ft_putendl_fd("Error: No collectibles found", STDERR_FILENO);
+		return (false);
+	}
+	return (true);
+}
+
+static bool	process_element(t_game *game, t_element_status *status)
+{
+	if (game->map[status->i][status->j] == 'P')
+		status->player_found = true;
+	else if (game->map[status->i][status->j] == 'E')
+		status->exit_found = true;
+	if (!update_element_count(game, game->map[status->i][status->j],
+			status->j, status->i))
+	{
+		ft_putendl_fd("Error: Duplicate player or exit", STDERR_FILENO);
+		return (false);
+	}
+	return (true);
+}
+
+bool	validate_elements(t_game *game)
+{
+	t_element_status	status;
+
+	if (!game || !game->map)
+	{
+		ft_putendl_fd("Error: Invalid game state", STDERR_FILENO);
+		return (false);
+	}
+	init_element_count(game);
+	status.player_found = false;
+	status.exit_found = false;
+	status.i = -1;
+	while (++status.i < game->height)
+	{
+		status.j = -1;
+		while (++status.j < game->width)
+		{
+			if (!process_element(game, &status))
+				return (false);
+		}
+	}
+	return (check_element_requirements(status.player_found,
+			status.exit_found, game));
 }
