bfb802fbaf6aa85f84c440ca908a491559bd2757
seungahpyun <spyun@student.codam.nl>
2025-01-09 15:42:01 +0100

    without number of meal, working:

---
+++ philosophers/bonus/include/philosophers_bonus.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philosophers_bonus.h                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spyun <spyun@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/09 14:38:55 by spyun         #+#    #+#                 */
+/*   Updated: 2025/01/09 14:49:33 by spyun         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_BONUS_H
+# define PHILOSOPHERS_BONUS_H
+
+# include <stdbool.h>
+# include <semaphore.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <signal.h>
+
+typedef struct s_philo
+{
+	int				id;
+	pid_t			pid;
+	int				meals_eaten;
+	long long		last_meal;
+	struct s_data	*data;
+}	t_philo;
+
+typedef struct s_data
+{
+	int			num_philos;
+	int			time_to_die;
+	int			time_to_eat;
+	int			time_to_sleep;
+	int			must_eat;
+	long long	start_time;
+	sem_t		*forks;
+	sem_t		*print_sem;
+	sem_t		*dead_sem;
+	t_philo		*philos;
+}	t_data;
+
+#endif
---
+++ philosophers/bonus/scr/main_bonus.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main_bonus.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spyun <spyun@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/01/09 14:40:18 by spyun         #+#    #+#                 */
+/*   Updated: 2025/01/09 14:49:46 by spyun         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "philosophers_bonus.h"
+
+static void	wait_for_processes(t_data *data)
+{
+	int	i;
+	int	status;
+
+	i = 0;
+	while (i < data->num_philos)
+	{
+		waitpid(-1, &status, 0);
+		if (WEXITSTATUS(status) == EXIT_FAILURE)
+		{
+			i = 0;
+			while (i < data->num_philos)
+			{
+				if (data->philos[i].pid > 0)
+					kill(data->philos[i].pid, SIGKILL);
+				i++;
+			}
+			break ;
+		}
+		i++;
+	}
+}
+
+static int	start_simulation(t_data *data)
+{
+	int		i;
+	pid_t	pid;
+
+	data->start_time = get_time();
+	i = 0;
+	while (i < data->num_philos)
+	{
+		pid = fork();
+		if (pid == -1)
+			return (error_msg("Fork failed"));
+		if (pid == 0)
+		{
+			philo_routine(&data->philos[i]);
+			exit(EXIT_SUCCESS);
+		}
+		data->philos[i].pid = pid;
+		i++;
+		usleep(100);
+	}
+	wait_for_processes(data);
+	return (EXIT_SUCCESS);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	memset(&data, 0, sizeof(t_data));
+	if (argc != 5 && argc != 6)
+		return (error_msg("Error: wrong number of arguments"));
+	if (!init_data(&data, argc, argv))
+		return (error_msg("Error: invalid arguments"));
+	if (!init_semaphores(&data))
+		return (error_cleanup("Error: semaphore initialization failed", &data));
+	if (!init_philos(&data))
+		return (error_cleanup("Error: philosopher initialization failed", &data));
+	if (start_simulation(&data) != EXIT_SUCCESS)
+		return (error_cleanup("Error: simulation failed", &data));
+	cleanup(&data);
+	return (EXIT_SUCCESS);
+}
