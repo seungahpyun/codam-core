16ab9709719dfdb1b6a222ae94e0858d76b39fb6
seungahpyun <spyun@student.codam.nl>
2025-03-06 11:52:56 +0100

    split into funtion

--- cpp-modules/cpp-module-00/ex00/megaphone.cpp
+++ cpp-modules/cpp-module-00/ex00/megaphone.cpp
@@ -3,14 +3,8 @@
 #include <string>
 #include <algorithm>
 
-int main(int argc, char **argv)
+void printUpper(int argc, char **argv)
 {
-	if (argc == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return 0;
-	}
-
 	for (int i = 1; i < argc; i++)
 	{
 		std::string str = argv[i];
@@ -19,6 +13,14 @@ int main(int argc, char **argv)
 		if (i < argc - 1)
 			std::cout << " ";
 	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+		printUpper(argc, argv);
 	std::cout << std::endl;
 	return 0;
 }
