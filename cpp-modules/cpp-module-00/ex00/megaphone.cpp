27b597af8f1c6b640fd7b7ed18f968c74c93386b
seungahpyun <spyun@student.codam.nl>
2025-03-06 12:02:44 +0100

    remove double new line

--- cpp-modules/cpp-module-00/ex00/megaphone.cpp
+++ cpp-modules/cpp-module-00/ex00/megaphone.cpp
@@ -18,7 +18,7 @@ void printUpper(int argc, char **argv)
 int main(int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 		printUpper(argc, argv);
 	std::cout << std::endl;
