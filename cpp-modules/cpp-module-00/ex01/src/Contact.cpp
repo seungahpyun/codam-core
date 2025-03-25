3a4cbae6c629a12719bff140bc3c31d2bf487b49
seungahpyun <spyun@student.codam.nl>
2025-03-25 10:18:17 +0100

    getter and setter

--- cpp-modules/cpp-module-00/ex01/src/Contact.cpp
+++ cpp-modules/cpp-module-00/ex01/src/Contact.cpp
@@ -6,7 +6,59 @@
 /*   By: spyun <spyun@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/25 10:05:57 by spyun         #+#    #+#                 */
-/*   Updated: 2025/03/25 10:05:58 by spyun         ########   odam.nl         */
+/*   Updated: 2025/03/25 10:17:44 by spyun         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Contact.hpp"
+
+void Contact::setFirstName(std::string firstName)
+{
+	_firstName = firstName;
+}
+
+void Contact::setLastName(std::string lastName)
+{
+	_lastName = lastName;
+}
+
+void Contact::setNickname(std::string nickname)
+{
+	_nickname = nickname;
+}
+
+void Contact::setMobileNumber(std::string mobileNumber)
+{
+	_mobileNumber = mobileNumber;
+}
+
+void Contact::setDarkestSecret(std::string darkestSecret)
+{
+	_darkestSecret = darkestSecret;
+}
+
+std::string Contact::getFirstName() const
+{
+	return _firstName;
+}
+
+std::string Contact::getLastName() const
+{
+	return _lastName;
+}
+
+std::string Contact::getNickname() const
+{
+	return _nickname;
+}
+
+std::string Contact::getMobileNumber() const
+{
+	return _mobileNumber;
+}
+
+std::string Contact::getDarkestSecret() const
+{
+	return _darkestSecret;
+}
 
